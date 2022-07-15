18b119220cfc8dbcf4580024c7d23f382aa3eaaa
Mindy Douglas <mindy.douglas@icloud.com>
2022-07-15 17:57:33 -0500

    Initial Commit

---
+++ Choices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Choices
+//
+//  Created by Mindy Douglas on 7/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
